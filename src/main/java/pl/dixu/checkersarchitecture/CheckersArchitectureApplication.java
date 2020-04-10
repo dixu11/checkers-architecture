f0fc923ad7d5021660c4a3ac432ee619c4dccf7b
Marcin Sowa <marcin.sowa121@gmail.com>
2020-04-10 15:35:36 +0200

    add initial project

---
+++ src/main/java/pl/dixu/checkersarchitecture/CheckersArchitectureApplication.java
@@ -0,0 +1,13 @@
+package pl.dixu.checkersarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckersArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckersArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dixu/checkersarchitecture/CheckersArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dixu.checkersarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckersArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
